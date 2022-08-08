96f6a201e863160e6d9b142cc63b92b9a23248fe
dokacoder <dokacoder@gmail.com>
2022-08-08 18:06:59 +0900

    Inline function refactoring

--- ch6/6-2 after.js
+++ ch6/6-2 after.js
@@ -1,20 +1,17 @@
+/* 
+함수를 분리시키는것만이 답은 아니다
+함수이름 자체가 코드인경우 합쳐서 의미를 한 눈에 파악하게 하는것도 좋음
+*/
+
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 // 예제 2
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
-}
-
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
+  const result = [];
+  result.push(["name", customer.name]);
+  result.push(["location", customer.location]);
+  return result;
 }
