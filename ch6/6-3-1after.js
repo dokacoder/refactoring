251cccc31c7f1c1433d933024f95d842b7ef6778
dokacoder <dokacoder@gmail.com>
2022-08-08 18:18:31 +0900

    Extract variable

--- ch6/6-3-1after.js
+++ ch6/6-3-1after.js
@@ -1,8 +1,13 @@
 export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  /* 
+   주석으로 해결하려고 하는 습관은 버려야함
+   주석을 지울수 있는지 늘 고민!
+   return 하고있는 내용이 뭘표현하는지 명확하기 때문에
+   가격(price) = 기본가격 - 수량할인 + 배송비 주석지워도 됨
+  */
+  const basePrice = order.quantity * order.itemPrice;
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return basePrice - discount + shipping;
 }
